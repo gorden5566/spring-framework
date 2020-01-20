e2b71b2530c4575dc356c671461cf3f68be626df
gorden5566 <gorden5566@126.com>
2020-01-20 17:13:30 +0800

    ConversionService：类型转换服务

--- spring-core/src/main/java/org/springframework/core/convert/ConversionService.java
+++ spring-core/src/main/java/org/springframework/core/convert/ConversionService.java
@@ -19,6 +19,8 @@ package org.springframework.core.convert;
 import org.springframework.lang.Nullable;
 
 /**
+ * 用于类型转换的接口
+ *
  * A service interface for type conversion. This is the entry point into the convert system.
  * Call {@link #convert(Object, Class)} to perform a thread-safe type conversion using this system.
  *
