9642aeeeb6f10394953403b89deacd7b79d26fc3
gorden5566 <gorden5566@126.com>
2020-01-20 16:53:36 +0800

    MutablePropertySources：PropertySources的默认实现类

--- spring-core/src/main/java/org/springframework/core/env/MutablePropertySources.java
+++ spring-core/src/main/java/org/springframework/core/env/MutablePropertySources.java
@@ -26,6 +26,8 @@ import java.util.stream.Stream;
 import org.springframework.lang.Nullable;
 
 /**
+ * PropertySources 接口的默认实现类
+ *
  * The default implementation of the {@link PropertySources} interface.
  * Allows manipulation of contained property sources and provides a constructor
  * for copying an existing {@code PropertySources} instance.
